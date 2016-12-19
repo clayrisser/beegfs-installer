38fa7911dda5b3353f2a85798800afba3d17b1ee
Jam Risser <jamrizzi@users.noreply.github.com>
2016-12-19 05:40:26 -0600

    Update download.sh

--- download.sh
+++ download.sh
@@ -13,7 +13,7 @@ fi
 unset UNAME
 
 if [ "$DISTRO" == "Ubuntu" ]; then
-    curl -L https://github.com/jamrizzi/beegfs-installer/releases/download/v0.0.3/beegfs-installer-ubuntu.tar.gz | tar zxv
+    curl -L https://github.com/jamrizzi/beegfs-installer/releases/download/v0.0.4/beegfs-installer-ubuntu.tar.gz | tar zxv
 elif [ "$DISTRO" == "CentOS" ]; then
-    curl -L https://github.com/jamrizzi/beegfs-installer/releases/download/v0.0.3/beegfs-installer-centos.tar.gz | tar zxv
+    curl -L https://github.com/jamrizzi/beegfs-installer/releases/download/v0.0.4/beegfs-installer-centos.tar.gz | tar zxv
 fi
